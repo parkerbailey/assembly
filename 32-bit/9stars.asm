f854e5e9b943f04cee502a5a0b9bbae4a37d4a57
Parker Bailey <parker@baileymail.me>
2025-03-11 21:46:32 -0400

    add section for returning newline character

--- 32-bit/9stars.asm
+++ 32-bit/9stars.asm
@@ -14,6 +14,12 @@ _start:                               ;tell linker entry point
 	mov eax, 4                        ;system call number (sys_write)
 	int 0x80                          ;call kernel
 
+	mov edx, 1                        ;newline length
+	mov ecx, nl                       ;newline contents
+	mov ebx, 1						  ;file descriptor (stdout)
+	mov eax, 4                        ;system call number (sys_write)
+	int 0x80                          ;call kernel
+
 	mov eax, 1                        ;system call number (sys_exit)
 	int 0x80                          ;call kernel
 
@@ -21,3 +27,4 @@ section .data
 msg db 'Displaying 9 stars', 0xa      ;a message
 len equ $ - msg                       ;calculate length of message
 s2 times 9 db '*'
+nl db 0xa                             ;insert newline at end
