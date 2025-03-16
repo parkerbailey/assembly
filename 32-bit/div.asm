c4a426105a8dae228e23111e7b75e75b4932a294
Parker Bailey <parker@baileymail.me>
2025-03-15 23:08:00 -0400

    add comments

--- 32-bit/div.asm
+++ 32-bit/div.asm
@@ -2,34 +2,34 @@ section .text
 	global _start
 
 _start:
-	mov ax, '8'
-	sub ax, '0'
+	mov ax, '8'             ; ASCII '8' to ax register
+	sub ax, '0'             ; convert to decimal
 
-	mov bl, '2'
-	sub bl, '0'
-	div bl
-	add ax, '0'
+	mov bl, '2'             ; ASCII '2' to bl register
+	sub bl, '0'             ; convert to decimal
+	div bl                  ; perform division
+	add ax, '0'             ; convert quotient back to ASCII
 
-	mov [res], ax
-	mov byte [res+1], 0xA
-	mov ecx, msg
-	mov edx, len
-	mov ebx, 1
-	mov eax, 4
+	mov [res], ax           ; mov quotient result from ax register to res value
+	mov byte [res+1], 0xA   ; add newline character to result
+	mov ecx, msg            ; move msg to ecx register
+	mov edx, len            ; move length to edx register
+	mov ebx, 1              ; stdout
+	mov eax, 4              ; sys_write
 	int 0x80
 
-	mov ecx, res
-	mov edx, 2
-	mov ebx, 1
-	mov eax, 4
+	mov ecx, res            ; move result variable to ecx register
+	mov edx, 2              ; move length 2 of result to edx register
+	mov ebx, 1              ; stdout
+	mov eax, 4              ; sys_write
 	int 0x80
 
-	mov eax, 1
+	mov eax, 1              ; exit
 	int 0x80
 
 section .data
-msg db 'The result is '
-len equ $ - msg
+msg db 'The result is '     ; define message
+len equ $ - msg             ; calcualte message length
 
 section .bss
-res resb 2
+res resb 2                  ; reserve 2 bytes for results
